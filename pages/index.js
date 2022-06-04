d5dcb5a7042422a5544f332b5e51d2de324879b2
Paolo Rollo <paolo.rollo1997@gmail.com>
2022-06-04 11:14:09 +0200

    first commit.

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>urbe.eth</title>
+        <meta name="description" content="Benvenuti in urbe.eth, la prima community romana che si occupa del web3!" />
+        <link rel="icon" href="/logo.png" />
+      </Head>
+
+      <div className='h-screen w-full flex flex-col items-center justify-center space-y-8'>
+        <img src='/logo.png' className='h-64 w-64' />
+        <h1 className='font-bold text-2xl animate-pulse'>Work in progress.</h1>
+      </div>
+    </div>
+  )
+}
